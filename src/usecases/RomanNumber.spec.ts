f30e97ecbc7e135538d702f593f48a497e657922
Emerson Santana <emersonfidelon@gmail.com>
2021-03-21 08:45:28 -0300

    test: shoukd understand numbers like IX

--- src/usecases/RomanNumber.spec.ts
+++ src/usecases/RomanNumber.spec.ts
@@ -1,45 +1,57 @@
 import { RomanNumber } from "./RomanNumber";
 
 describe('RomanNumber', () => {
-  test('should know I symbol', () => {
-    const roman = new RomanNumber();
-    const number = roman.convert('I');
-    expect(1).toBe(number);
-  });
-
-  test('should know V symbol', () => {
-    const roman = new RomanNumber()
-    const number = roman.convert('V')
-    expect(5).toBe(number)
-  });
-
-  test('should know L symbol', () => {
-    const roman = new RomanNumber()
-    const number = roman.convert('L')
-    expect(50).toBe(number)
-  });
-
-  test('should know two symbols, for example II', () => {
-    const roman = new RomanNumber()
-    const number = roman.convert('II')
-    expect(2).toBe(number)
-  });
-
-  test('should know two symbols, for example XX', () => {
-    const roman = new RomanNumber()
-    const number = roman.convert('XX')
-    expect(20).toBe(number)
-  });
-
-  test('should understand four symbols, two by two, for example XXII', () => {
-    const roman = new RomanNumber()
-    const number = roman.convert('XXII')
-    expect(22).toBe(number)
-  });
-
-  test('should understand four symbols, two by two, for example XIII', () => {
-    const roman = new RomanNumber()
-    const number = roman.convert('XIII')
-    expect(13).toBe(number)
+  describe('convert()', () => {
+    test('should know I symbol', () => {
+      const roman = new RomanNumber();
+      const number = roman.convert('I');
+      expect(1).toBe(number);
+    });
+  
+    test('should know V symbol', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('V')
+      expect(5).toBe(number)
+    });
+  
+    test('should know L symbol', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('L')
+      expect(50).toBe(number)
+    });
+  
+    test('should know two symbols, for example II', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('II')
+      expect(2).toBe(number)
+    });
+  
+    test('should know two symbols, for example XX', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('XX')
+      expect(20).toBe(number)
+    });
+  
+    test('should understand four symbols, two by two, for example XXII', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('XXII')
+      expect(22).toBe(number)
+    });
+  
+    test('should understand numbers like XIII', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('XIII')
+      expect(13).toBe(number)
+    });
+  
+    test('should understand numbers like IX', () => {
+      const roman = new RomanNumber()
+      const number = roman.convert('IX')
+      const number2 = roman.convert('IV')
+      const number3 = roman.convert('XIX')
+      expect(9).toBe(number)
+      expect(4).toBe(number2)
+      expect(19).toBe(number3)
+    });
   });
 });
