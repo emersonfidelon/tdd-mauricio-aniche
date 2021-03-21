fee74c1d9e3414ed56e37a80d3735ff1b169a5b6
Emerson Santana <emersonfidelon@gmail.com>
2021-03-21 08:11:15 -0300

    test: replace class to know two symbols

--- src/usecases/RomanNumber.spec.ts
+++ src/usecases/RomanNumber.spec.ts
@@ -18,4 +18,10 @@ describe('Name of the group', () => {
     const number = roman.convert('L')
     expect(50).toBe(number)
   });
+
+  test('should know two symbols, for example II', () => {
+    const roman = new RomanNumber()
+    const number = roman.convert('II')
+    expect(2).toBe(number)
+  });
 });
